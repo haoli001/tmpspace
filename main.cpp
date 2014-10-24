07f6e86260c781260174960f399cf70d20a9b74f
75:c4:25:96:f6:36:92:c6:43:09:63:4e:92:57:38:de <lh441027725@gmail.com>
2014-10-24 11:25:12 +0800

    晶振控制时间

--- main.cpp
+++ main.cpp
@@ -1,79 +1,32 @@
 #include <reg51.h>
-
-sbit KEY=P3^3;
+#define SYSCLK 11059200
+#define TCNT 200
 
 bit flag1=0;
-void init_int1(){
-    
-    IT1=0;
-    EA=1;
-    
-    EX1=1;
-    
+void init_intT1(){
+    TMOD&=0X10;
+    TH1=(65536-SYSCLK/12/TCNT)/256;
+    TL1=(65536-SYSCLK/12/TCNT)%256;
+    TR1=1;
 }
 
-
-
-void delay(unsigned int i){
-    
-    for(; i>0;i--);
-    
-}
-
-int  f(int tmpp,int flag){
-    
-    P2=tmpp;
-    int tmppp=tmpp;
-    delay(5000);
-    if(!flag){
-        
-        //tmpp<<=1;
-        tmpp-=tmppp&0xf;
-        tmpp+=tmppp&0xf0;
-        
-        //if(tmpp!=0xfe)tmpp|=1;
-        if(!(tmpp&0xff))tmpp=0x7e;
-        
-    }
-    
-    else  {
-        
-        //tmpp>>=1;
-        tmpp+=tmppp&0xf;
-        tmpp-=tmppp&0xf0;
-        
-        //if(tmpp!=0x7f)tmpp|=0x80; }
-        if((tmpp&0xff)==0x7e)tmpp=0xe7;
-    
-    return tmpp&0xff;
-    
-}
-
-void int1_service(void) interrupt 2{
-    
-    delay(1000);
-    
-    if(KEY==0){
-        EX1=0;
-        flag1=1;
-        
-    }
-    
-}
 int main(){
     
-    int flag=0;
-    unsigned int tmp=0x18;
-    P2=0; 
-    init_int1();
+    unsigned int i=0;
+    P2=0;
+    init_inT1();
     while(1){
-        tmp=f(tmp,flag);
-        if(flag1){ 	
-            while(KEY==0)tmp=f(tmp,flag);
-            flag=flag^1;
-            flag1=0;
-            EX1=1;			
+        while(TF1==0);//time =1 go
+        TF1=0;
+        TH1=(65536-SYSCLK/12/TCNT)/256;
+        TL1=(65536-SYSCLK/12/TCNT)%256;
+        i++;
+        if(i==TCNT){//1s
+            P2=~P2;
+            i=0;
+            
         }
+        
     }
     return 0;
     
